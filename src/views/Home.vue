c5dcfa429c222d010c96b2800f4fa5ba5b116b50
Adam <aswilson14@gmail.com>
2021-04-22 22:23:11 -0400

    copy html template to frontend

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    
+     <h2>{{ blurb.location }}</h2> 
+     <img v-bind:src="blurb.image_url" />
+     <p>{{ blurb.fact }}</p>
+     
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      blurb: {},
+      message: "BlurbZilla",
+    };
+  },
+  created: function () {
+    this.indexBlurbs();
+  },
+  methods: {
+    indexBlurbs: function () {
+      axios.get("api/blurbs").then((response) => {
+        console.log("blurbs index", response);
+        this.blurb = response.data;
+      });
+    },
+  },
+};
+</script>
